a544b34e44f23e873e2ffe31e01694a5548d226f
Thulasiram S <84460719+Thulazy@users.noreply.github.com>
2025-07-21 17:44:56 +0530

    ds

---
+++ DSA/Problems/Sortings/InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Problems.Sortings
+{
+    public class InsertionSort
+    {
+        public void SortArrayUsingInsertionSort(List<int> nums)
+        {
+            Console.WriteLine("Unsorted Array (Insertion Sort)");
+            foreach (var items in nums)
+            {
+                Console.WriteLine(items);
+            }
+            int len = nums.Count();
+            for (int i = 1; i < len; i++)
+            {
+                int key = nums[i];
+                int prevIndex = i - 1;
+                while (prevIndex >= 0 && nums[prevIndex] > key)
+                {
+                    nums[prevIndex + 1] = nums[prevIndex];
+                    prevIndex--;
+                }
+                nums[prevIndex + 1] = key;
+            }
+            Console.WriteLine("sorted Array (Insertion Sort)");
+            foreach (var items in nums)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,4 @@
-﻿using DSA.Problems.Easy;
+﻿﻿using DSA.Problems.Easy;
 using DSA.Problems.Sortings;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +9,10 @@ namespace DSA
     {
         static async Task Main(string[] args)
         {
-            int[] nums = { 4, 6, 2, 4, 6, 1 };
-            SelectionSort s = new SelectionSort();
-            s.SortArrayUsingSelectionSort(nums);
+            int[] nums = { 4, 1, 3, 9, 7 };
+            List<int> numsList = nums.ToList();
+            InsertionSort s = new InsertionSort();
+            s.SortArrayUsingInsertionSort(numsList);
         }
     }
 }
