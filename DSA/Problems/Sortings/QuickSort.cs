c6409e783c95e9f11b5d00cd866d5c342b4e61c0
Thulasiram S <84460719+Thulazy@users.noreply.github.com>
2025-07-22 20:55:01 +0530

    adf

---
+++ DSA/Problems/Sortings/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Problems.Sortings
+{
+    public class QuickSort
+    {
+        public void SortArrayUsingQuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                {
+                    SortArrayUsingQuickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    SortArrayUsingQuickSort(arr, pivot + 1, right);
+                }
+            }
+        }
+
+        public int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+                if (arr[left] < pivot)
+                {
+                    left++;
+                }
+                if (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    int temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -11,8 +11,8 @@ namespace DSA
         {
             int[] nums = { 8, 2, 5, 3, 4, 7, 6, 1 };
             List<int> numsList = nums.ToList();
-            MergeSort s = new MergeSort();
-            s.SortArrayUsingMergeSort(nums);
+            QuickSort s = new QuickSort();
+            s.SortArrayUsingQuickSort(nums, 0 , nums.Length - 1);
             for (int i = 0; i < nums.Length; i++)
             {
                 Console.Write(nums[i] + " ");
